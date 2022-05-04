1ba4394747e1237e70d2561842ed2f770a69dca3
Jana <jana@codi.coop>
2022-05-04 08:36:20 +0200

    Fix: createsuperuser ara SÍ que no peta

---
+++ src/apps/users/management/commands/auto_superuser.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from apps.users.models import User
+
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+
+class Command(BaseCommand):
+    help = "Crea el superuser predeterminat sempre que no existeixi ja."
+
+    def handle(self, *args, **options):
+        DJANGO_SUPERUSER_EMAIL = env("DJANGO_SUPERUSER_EMAIL")
+        DJANGO_SUPERUSER_PASSWORD = env("DJANGO_SUPERUSER_PASSWORD")
+
+        if not User.objects.filter(email=DJANGO_SUPERUSER_EMAIL).exists():
+            User.objects.create_superuser(email=DJANGO_SUPERUSER_EMAIL,
+                                          password=DJANGO_SUPERUSER_PASSWORD)
+
+        return 0
