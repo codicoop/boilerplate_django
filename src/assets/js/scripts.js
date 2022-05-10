b8b4556498acfc1858accf947f447d0736be64e6
MartaRF3 <ricartfm@gmail.com>
2022-05-10 17:10:21 +0200

    Base maquetada

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,10 @@
+// Menu functionality
+const header = document.querySelector('.header')
+
+function openMenu(){
+  header.classList.add('is-open')
+}
+
+function closeMenu(){
+  header.classList.remove('is-open')
+}
