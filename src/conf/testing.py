2bbe47a2f36a96293f052094ad1701eb247f4b24
Pere Picornell <pere@perepicornell.com>
2022-10-18 15:46:58 +0200

    Added WHITENOISE_AUTOREFRESH in tox. Without that you would need to do collectstatic in the test environment, or enable debug. Otherwise, Whitenoise will throw an error.

--- src/conf/testing.py
+++ src/conf/testing.py
@@ -3,3 +3,4 @@ from .settings import *  # noqa
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:", "TEST": {}}
 }
+WHITENOISE_AUTOREFRESH = env.bool("WHITENOISE_AUTOREFRESH", default=DEBUG)
