be858be5ecabbf97fe644c71dbdc31a93dac0135
Jana <jana@codi.coop>
2022-11-22 05:24:21 +0100

    Ampliació de .env.example i settings.py
    Ampliació de la documentació en comentaris de les variables d'entorn, tan a .env.example com a settings.py.

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -1,10 +1,11 @@
 """
-Local settings through django-environ
-https://django-environ.readthedocs.io/en/latest/
+Settings for the Django project.
 
-Put the settings in /conf/.env
+For more information on Django's settings, visit:
+    https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
+from pathlib import Path
+import logging
 
 import environ
 import sentry_sdk
@@ -15,35 +16,65 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env()
 
-##########################
-#         Sentry         #
-##########################
-sentry_sdk.init(
-    dsn=env("SENTRY_DSN", default=""),
-    integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# False if not in os.environ
+################################################################################
+#                               General                                        #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#debug
 DEBUG = env("DEBUG", default=False)
-# Raises django's ImproperlyConfigured exception if SECRET_KEY not in
-# os.environ
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#secret-key
 SECRET_KEY = env.str("SECRET_KEY", default=get_random_secret_key())
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
 # Instance's absolute URL (given we're not using Sites framework)
 ABSOLUTE_URL = env.str("ABSOLUTE_URL", default="")
 
-# Variables for non-interactive superuser creation
-DJANGO_SUPERUSER_EMAIL = env("DJANGO_SUPERUSER_EMAIL", default="")
-DJANGO_SUPERUSER_PASSWORD = env("DJANGO_SUPERUSER_PASSWORD", default="")
+# https://docs.djangoproject.com/en/4.1/ref/settings/#silenced-system-checks
+SILENCED_SYSTEM_CHECKS = ["models.W042"]
 
-# Database
+# https://github.com/fabiocaccamo/django-maintenance-mode#settings
+MAINTENANCE_MODE = env.bool("MAINTENANCE_MODE", default=False)
+MAINTENANCE_MODE_STATE_BACKEND = "maintenance_mode.backends.DefaultStorageBackend"
+
+
+################################################################################
+#                Internationalization and localization                         #
+################################################################################
+
+# Local time zone. Choices are
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# though not all of them may be available with every OS.
+# In Windows, this must be set to your system time zone.
+TIME_ZONE = "Europe/Andorra"
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#language-code
+LANGUAGE_CODE = "en"
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#languages
+LANGUAGES = [
+    ("en", _("English")),
+    ("ca", _("Catalan")),
+]
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#use-i18n
+USE_I18N = True
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-USE_TZ
+USE_TZ = True
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-LOCALE_PATHS
+LOCALE_PATHS = [str(BASE_DIR / "locale")]
+
+
+################################################################################
+#                               Databases                                      #
+################################################################################
+
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     "default": {
@@ -55,53 +86,29 @@ DATABASES = {
         "PORT": env("DB_PORT", default=5432),
     }
 }
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 # If you enable this try a makemigrations to make sure the third party
 # packages are not generating migrations.
 # DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Sendgrid
-SENDGRID_API_KEY = env("SENDGRID_API_KEY", default="")
-SENDGRID_SANDBOX_MODE_IN_DEBUG = env(
-    "SENDGRID_SANDBOX_MODE_IN_DEBUG", bool, default=False
-)
-SENDGRID_TRACK_EMAIL_OPENS = env("SENDGRID_TRACK_EMAIL_OPENS", bool, default=False)
-SENDGRID_TRACK_CLICKS_HTML = env("SENDGRID_TRACK_CLICKS_HTML", bool, default=False)
-SENDGRID_TRACK_CLICKS_PLAIN = env("SENDGRID_TRACK_CLICKS_PLAIN", bool, default=False)
 
-# SMTP
-EMAIL_HOST = env.str("EMAIL_HOST", default="")
-EMAIL_PORT = env.int("EMAIL_PORT", default="")
-EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default="")
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
-EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
-EMAIL_BACKEND = env.str(
-    "EMAIL_BACKEND",
-    default="django.core.mail.backends.console.EmailBackend",
-)
+################################################################################
+#                                  URLs                                        #
+################################################################################
 
-# Redis
-REDIS_URL = env.str("REDIS_URL", "redis://redis:6379/0")
+# https://docs.djangoproject.com/en/4.1/ref/settings/#root-urlconf
+ROOT_URLCONF = "conf.urls"
 
-# Celery
-CELERY_BROKER_URL = REDIS_URL
+# https://docs.djangoproject.com/en/4.1/ref/settings/#wsgi-application
+WSGI_APPLICATION = "conf.wsgi.application"
 
-"""
-Django settings for conf project.
 
-Generated by 'django-admin startproject' using Django 2.2.9.
+################################################################################
+#                                  Apps                                        #
+################################################################################
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/2.2/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Application definition
+# https://docs.djangoproject.com/en/4.1/ref/settings/#installed-apps
 INSTALLED_APPS = [
     "maintenance_mode",
     "apps.base",
@@ -123,8 +130,52 @@ INSTALLED_APPS = [
     "django_extensions",
 ]
 
+
+################################################################################
+#                             Authentication                                   #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 
+# https://docs.djangoproject.com/en/4.1/ref/settings/#login-url
+LOGIN_URL = reverse_lazy("registration:login")
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#login-redirect-url
+LOGIN_REDIRECT_URL = reverse_lazy("registration:profile_details")
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#logout-redirect-url
+LOGOUT_REDIRECT_URL = "/"
+
+# Setting for the LoginRequiredMiddleware middleware
+# Using paths instead of view names so we can whitelist entire sections.
+# https://github.com/CleitonDeLima/django-login-required-middleware#quick-start
+LOGIN_REQUIRED_IGNORE_PATHS = [
+    r"^/admin/",
+    r"^/",
+]
+
+
+################################################################################
+#                               Passwords                                      #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+
+################################################################################
+#                               Middleware                                     #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-MIDDLEWARE
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -140,8 +191,29 @@ MIDDLEWARE = [
     "maintenance_mode.middleware.MaintenanceModeMiddleware",
 ]
 
-ROOT_URLCONF = "conf.urls"
 
+################################################################################
+#                                Static                                        #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#static-root
+STATIC_ROOT = str(BASE_DIR / "static")
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#static-url
+STATIC_URL = "/static/"
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#staticfiles-dirs
+STATICFILES_DIRS = [str(BASE_DIR / "assets")]
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#staticfiles-storage
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+################################################################################
+#                               Templates                                      #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#templates
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -161,63 +233,13 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "conf.wsgi.application"
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa
-    },
-]
+################################################################################
+#                                  Email                                       #
+################################################################################
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-LANGUAGE_CODE = "en"
-LANGUAGES = [
-    ("en", _("English")),
-    ("ca", _("Catalan")),
-]
-TIME_ZONE = "Europe/Andorra"
-USE_I18N = True
-USE_TZ = True
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, "locale"),
-]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
-]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-# For LoginRequiredMiddleware
-# Using paths instead of view names so we can whitelist entire sections.
-LOGIN_REQUIRED_IGNORE_PATHS = [
-    r"^/admin/",
-    r"/",
-]
-
-
-# Important settings, adjust according to your URLs:
-LOGIN_URL = reverse_lazy("registration:login")
-LOGIN_REDIRECT_URL = reverse_lazy("registration:profile_details")
-LOGOUT_REDIRECT_URL = "/"
-
-# Django Post Office
+# Post Office
+# https://github.com/ui/django-post_office#settings
 POST_OFFICE = {
     "BACKENDS": {
         "default": env(
@@ -230,19 +252,146 @@ POST_OFFICE = {
     "MESSAGE_ID_FQDN": env("POST_OFFICE_MESSAGE_ID_FQDN", default="example.com"),
     "CELERY_ENABLED": env("POST_OFFICE_CELERY_ENABLED", bool, default=False),
 }
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default=None)
 
+# Sendgrid
+# https://github.com/sklarsa/django-sendgrid-v5#other-settings
+SENDGRID_API_KEY = env("SENDGRID_API_KEY", default="")
+SENDGRID_SANDBOX_MODE_IN_DEBUG = env(
+    "SENDGRID_SANDBOX_MODE_IN_DEBUG", bool, default=True
+)
+SENDGRID_ECHO_TO_STDOUT = env("SENDGRID_ECHO_TO_STDOUT", bool, default=False)
+SENDGRID_TRACK_EMAIL_OPENS = env("SENDGRID_TRACK_EMAIL_OPENS", bool, default=False)
+SENDGRID_TRACK_CLICKS_HTML = env("SENDGRID_TRACK_CLICKS_HTML", bool, default=False)
+SENDGRID_TRACK_CLICKS_PLAIN = env("SENDGRID_TRACK_CLICKS_PLAIN", bool, default=False)
+
+# SMTP
+# These are set to false as default given that Sendgrid's Web API is the default
+# https://docs.djangoproject.com/en/4.1/ref/settings/#email
+EMAIL_HOST = env.str("EMAIL_HOST", default="")
+EMAIL_PORT = env.int("EMAIL_PORT", default="")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default="")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
+EMAIL_BACKEND = env.str(
+    "EMAIL_BACKEND",
+    default="django.core.mail.backends.console.EmailBackend",
+)
+
+
+################################################################################
+#                                  Admin                                       #
+################################################################################
+
+# Credentials for the initial superuser. Leave empty to skip its creation.
+DJANGO_SUPERUSER_EMAIL = env("DJANGO_SUPERUSER_EMAIL", default="")
+DJANGO_SUPERUSER_PASSWORD = env("DJANGO_SUPERUSER_PASSWORD", default="")
 
 # Grappelli
+# https://django-grappelli.readthedocs.io/en/latest/customization.html#available-settings
 GRAPPELLI_INDEX_DASHBOARD = "apps.base.dashboard.CustomIndexDashboard"
 
 # Constance
+# https://django-constance.readthedocs.io/en/latest/#configuration
 CONSTANCE_BACKEND = "constance.backends.database.DatabaseBackend"
 CONSTANCE_CONFIG = {"PROJECT_NAME": ("", _("Name of the website."))}
 
-# Maintenance mode
-MAINTENANCE_MODE = env.bool("MAINTENANCE_MODE", default=False)
-MAINTENANCE_MODE_STATE_BACKEND = "maintenance_mode.backends.DefaultStorageBackend"
 
-# Silenced system checks
-SILENCED_SYSTEM_CHECKS = ["models.W042"]
+################################################################################
+#                                  Logging                                     #
+################################################################################
+
+# https://docs.djangoproject.com/en/4.1/ref/logging/
+if DEBUG == "off":
+    LOGGING_CONFIG = None
+    # Get loglevel from env
+    LOGLEVEL = env.str("DJANGO_LOGLEVEL", "info").upper()
+
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s",  # noqa
+                },
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "console",
+                },
+            },
+            "loggers": {
+                "": {
+                    "level": LOGLEVEL,
+                    "handlers": [
+                        "console",
+                    ],
+                },
+            },
+        }
+    )
+
+
+################################################################################
+#                                  Celery                                      #
+################################################################################
+
+# We're using the only instance of Redis, but if we use caching in the future,
+# we might want to set up two Redis servers and this will need to change.
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-broker_url
+CELERY_BROKER_URL = env.str("REDIS_URL", "redis://redis:6379/0")
+
+
+################################################################################
+#                            Django Rest Framework                             #
+################################################################################
+
+# https://www.django-rest-framework.org/api-guide/settings/
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+}
+
+# https://drf-spectacular.readthedocs.io/en/latest/settings.html
+SPECTACULAR_SETTINGS = {
+    # General schema metadata. Refer to spec for valid inputs
+    # https://spec.openapis.org/oas/v3.0.3#openapi-object
+    "TITLE": "django_boilerplate API",
+    "DESCRIPTION": "API for the React front-end of the CNJC app.",
+    "VERSION": "1.0.0",
+    "TAGS": [
+        {
+            "name": "Hour registry",
+            "description": "Endpoints to register and access and employee worked days",
+            "externalDocs": {
+                "description": "Taiga Wiki",
+                "url": "https://tree.taiga.io/project/cooperatiu-cnjc/wiki/home",
+            },
+        }
+    ],
+}
+
+################################################################################
+#                                Sentry                                        #
+################################################################################
+
+# https://docs.sentry.io/platforms/python/guides/django/
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN", default=""),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
