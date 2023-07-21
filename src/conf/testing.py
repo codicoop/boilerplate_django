98359c88ea076f4259de9ed3addd6eb4c26ed6cc
Jana <jana@codi.coop>
2023-07-21 22:34:16 +0200

    Eliminat pytest de tox

--- src/conf/testing.py
+++
@@ -1,6 +0,0 @@
-from .settings import *  # noqa
-
-DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:", "TEST": {}}
-}
-WHITENOISE_AUTOREFRESH = env.bool("WHITENOISE_AUTOREFRESH", default=DEBUG)
