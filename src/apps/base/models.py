286eb720a4e7b8c5729c3569f08f70df2ef8bc5b
Pere Picornell <pere@perepicornell.com>
2024-01-30 13:24:53 +0100

    Canvi al nom d'un camp.

--- src/apps/base/models.py
+++ src/apps/base/models.py
@@ -27,7 +27,7 @@ class BaseModel(SetBooleanDatetimeMixin, models.Model):
         blank=True,
         related_name="%(app_label)s_%(class)s_related",
         on_delete=models.CASCADE,
-        verbose_name=_("author"),
+        verbose_name=_("created by"),
     )
     updated_at = models.DateTimeField(auto_now=True, null=False, editable=False)
 
