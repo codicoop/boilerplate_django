4ac729d3104e4e9e492e6d54d95f75ad217486fc
jvncode <jvncode@gmail.com>
2024-03-26 17:54:45 +0100

    Models and forms

--- src/apps/demo/views.py
+++ src/apps/demo/views.py
@@ -1,9 +1,40 @@
-from django.views.generic import FormView
+from django.shortcuts import render, redirect
 
-from src.apps.demo.forms import DataForm
+from apps.demo.forms import DataForm
+from apps.demo.models import Data
 
 
-class DataView(FormView):
-    template_name = "registration/home.html"
-    form_class = DataForm
-    success_url = ""
+def data_view(request):
+    if request.method == 'GET':
+        form = DataForm()
+    else:
+        form = DataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, "home.html", {'form': form})
+
+
+def list_view(request):
+    context = {}
+    context["data"] = Data.objects.all()
+    return render(request, "list_view.html", context)
+
+
+def detail_view(request, id):
+    context = {}
+    context["data"] = Data.objects.get(id=id)
+    return render(request, "detail_view.html", context)
+
+
+def update_view(request, id):
+    if request.method == 'GET':
+        context = {}
+        context["data"] = Data.objects.get(id=id)
+        return render(request, "update_view.html", context)
+    else:
+        form = DataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        return render(request, "home.html", {'form': form})
