ad2d2ddf96f1056e2c183146fee0911125a73aa0
MartaRF3 <ricartfm@gmail.com>
2024-03-26 10:00:08 +0100

    Esquelet demo form

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,61 @@ module.exports = {
     './src/templates/**/*.html',
     './node_modules/flowbite/**/*.js'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "50":"#d9eddf",
+          "100":"#eff6ff",
+          "200":"#10dda3",
+          "300":"#00bfa9",
+          "400":"#00a1a4",
+          "500":"#008293",
+          "600":"#1e6479",
+          "700":"#2f4858",
+          "800":"#386c5f",
+          "900":"#005139",
+          "950":"#005139"
+        }
+      }
+    },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
   },
   plugins: [
     require('flowbite/plugin')
