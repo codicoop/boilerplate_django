7930e8685ec65856697c41d32282d702e2ee06dd
Pere Picornell <pere@perepicornell.com>
2024-04-26 17:14:57 +0200

    Fixed: en cas d'intentar resoldre una URL falsa, la funció que retorna les URLs dels idiomes ara retornarà una de vàlida per defecte (s'ha hardcodejat home)

--- src/project/templatetags/language.py
+++ src/project/templatetags/language.py
@@ -1,5 +1,5 @@
 from django.template import Library
-from django.urls import resolve, reverse
+from django.urls import Resolver404, resolve, reverse
 from django.utils.translation import activate, get_language
 
 register = Library()
@@ -12,12 +12,25 @@ def change_lang(context, lang=None, *args, **kwargs):
     Usage: {% change_lang 'en' %}
     """
 
+    # When generating the language change url, it won't be able to generate it
+    # for a non-existent URL.
+    # For instance, if you access http://localhost:1234/asdasdadsad, the 404
+    # template shown contains the language selection menu, therefore, will be
+    # trying to generate the localized version of this URL.
+    # In these kind of situations, it can happen that request is not in
+    # context, and also that resolve() raises a Resolver404.
+    # For this reason, we set a fallback default URL, and return it if
+    # anything fails.
+    default_url = "home"
     request = context.get("request", None)
     if not request:
-        return reverse("home")
+        return reverse(default_url)
 
-    path = context["request"].path
-    url_parts = resolve(path)
+    path = request.path
+    try:
+        url_parts = resolve(path)
+    except Resolver404:
+        return reverse(default_url)
 
     url = path
     cur_language = get_language()
