21039f89ad64805e9df1bf758b2bf40c7b215500
Pere Picornell <pere@perepicornell.com>
2024-10-29 12:27:16 +0100

    feat: django-extra-settings installed and set up, DEFAULT_PROJECT_NAME replaced by PROJECT_NAME.

--- src/project/settings.py
+++ src/project/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = [
     "phonenumber_field",
     "active_link",
     "sorl.thumbnail",
+    "extra_settings",
     "apps.users",
     "project",
     "apps.demo",
@@ -354,6 +355,31 @@ EMAIL_BACKEND = env.str(
 # Variables for non-interactive superuser creation
 DJANGO_SUPERUSER_EMAIL = env("DJANGO_SUPERUSER_EMAIL", default=None)
 DJANGO_SUPERUSER_PASSWORD = env("DJANGO_SUPERUSER_PASSWORD", default=None)
+
+################################################################################
+#                         django-extra-settings                                #
+################################################################################
+
+PROJECT_NAME = env.str("PROJECT_NAME", default="")
+EXTRA_SETTINGS_DEFAULTS = [
+    {
+        "name": "PROJECT_NAME",
+        "type": "Setting.TYPE_STRING",
+        "value": PROJECT_NAME,
+        "description": _("This name will be used for the HTML title of the "
+                        "public app, logo alt text and other places.")
+    },
+    {
+        "name": "LOGO",
+        "type": "Setting.TYPE_FILE",
+        "value": "",
+        "description": _("Logo image that will be used to customize the "
+                         "public app, e-mail templates and other.")
+    },
+]
+EXTRA_SETTINGS_IMAGE_UPLOAD_TO = "django-extra-settings-images"
+EXTRA_SETTINGS_FILE_UPLOAD_TO = "django-extra-settings-files"
+EXTRA_SETTINGS_VERBOSE_NAME = _("Dynamic settings")
 
 ################################################################################
 #                                  Logging                                     #
