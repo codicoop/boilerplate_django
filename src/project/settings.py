92293b1eb2291936f5a8fdeb647d7c92239cff35
Pere Picornell <pere@perepicornell.com>
2025-01-14 11:42:15 +0100

    Fix: Env var OVERRIDE_RECIPIENTS was not captured in settings.

--- src/project/settings.py
+++ src/project/settings.py
@@ -313,6 +313,11 @@ POST_OFFICE = {
     "MESSAGE_ID_ENABLED": True,
     "MESSAGE_ID_FQDN": env("POST_OFFICE_MESSAGE_ID_FQDN", default="example.com"),
     "CELERY_ENABLED": env("POST_OFFICE_CELERY_ENABLED", bool, default=False),
+    "OVERRIDE_RECIPIENTS": env.list(
+        "POST_OFFICE_OVERRIDE_RECIPIENTS",
+        list,
+        default=None,
+    ),
 }
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-from-email
